8297bbf8c188d0992ee7358266393f25a6c1c410
Birk Høgh-Mogensen <birk0169@edu.campusvejle.dk>
2021-06-02 13:53:27 +0200

    Added favorites

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,28 @@
     </header>
     <main>
       <section class="px-10 pr-0 mt-10">
-          <h3 class="font-semibold text-gray-500 dark:text-gray-300">New recipes</h3>
+          <h3 class="font-semibold text-gray-700 dark:text-gray-300">New recipes</h3>
           <Slideshow >
-            <BaseCard v-for="recipe in newRecipes" :key="recipe.id" class="card" :title="recipe.name" :time="recipe.time" :image="recipe.featuredImage"/>
+            <BaseCard v-for="recipe in newRecipes" 
+              :key="recipe.id" class="card" 
+              :title="recipe.name" 
+              :time="recipe.time" 
+              :image="recipe.featuredImage"/>
           </Slideshow>
       </section>
       <section class="px-10 mt-10">
-          <h3 class="font-semibold text-gray-500">Favorites</h3>
-
+          <h3 class="font-semibold text-gray-700">Favorites</h3>
+          <div class="mt-6" id="favorites">
+            <BaseCard v-for="recipe in favRecipes" 
+              :key="recipe.id" 
+              :title="recipe.name"
+              :time="recipe.time"
+              :image="recipe.featuredImage"
+              :isFavorite="recipe.isFavorite === 'true'"
+              :isVegan="recipe.isVegan === 'true'"
+              mode="focus"
+              height="h-60" />
+          </div>
       </section>
     </main>
   </div>
@@ -41,6 +55,7 @@
         this.recipes = data;
         console.log(this.recipes);
         this.sortRecipesByDate();
+        this.sortRecipesByFavorite();
       });
     },
     components: {
@@ -52,17 +67,22 @@
       return {
         recipes: {},
         newRecipes: {},
+        favRecipes: {}
       };
     },
     methods: {
       sortRecipesByDate: function () {
         this.newRecipes = this.recipes;
-        this.newRecipes.sort(function(a,b){
+        this.newRecipes.filter(function(a,b){
           return new Date(a.date) - new Date(b.date)
         });
         this.newRecipes.reverse();
-         this.newRecipes = this.newRecipes.slice(0, 5);
-      }    
+        //  this.newRecipes = this.newRecipes.slice(0, 5);
+      },
+      sortRecipesByFavorite() {
+        this.favRecipes = this.recipes.filter(r => r.isFavorite === 'true');
+        // this.favRecipes.reverse();
+      }
     }
   }
 
@@ -85,6 +105,7 @@
   color: #2c3e50;
   margin-top: 60px;
 }
+
 .card {
     display: inline-block !important;
     width: 300px;
