e9ab7e4b3a2d1b9c679e869a09d44435715fc36f
Birk Høgh-Mogensen <birk0169@edu.campusvejle.dk>
2021-06-07 11:53:30 +0200

    added routing

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import './assets/tailwind.css'
 import './index.css'
 
-createApp(App).mount('#app')
+import router from './router.js';
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app')
